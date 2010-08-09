1d550ffaa8c00849f26c8f03f24b1ad3eb229337
jzaefferer <joern.zaefferer@gmail.com>
2010-08-09 18:49:19 +0200

    Ref impl.

---
+++ jquery.edible-ref.js
@@ -0,0 +1,23 @@
+(function($) {
+	
+$.widget("cgnjs.edible", {
+	_create: function() {
+		var self = this;
+		this.element.bind("click.edible", function(event) {
+			self.element.hide();
+			self.input.val($.trim(self.element.text())).show().focus();
+		});
+		self.input = $("<input/>")
+			.insertAfter(self.element)
+			.hide()
+			.blur(function() {
+				$(this).hide();
+				self.element.show();
+			})
+			.keyup(function(event) {
+				self.element.text(this.value);
+			});
+	}
+});
+
+})(jQuery);
